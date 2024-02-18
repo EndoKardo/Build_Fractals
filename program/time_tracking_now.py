6d5eea7875999d073a1359715146610e7ea0d75d
EndoKardo <159020848+EndoKardo@users.noreply.github.com>
2024-02-18 14:54:16 +0300

    Update time_tracking_now.py

--- program/time_tracking_now.py
+++ program/time_tracking_now.py
@@ -1,1 +1,6 @@
+class Time_tracking_now:
+  def __init__(self):
+    # Инициализация модуля тайм-трекинга
 
+  def time_tracking_build_fractl(self):
+    # Модуль отслеживания времени выполнения построения фрактала
