17e24e579958056aefdae549494c71bebe509a8f
EndoKardo <159020848+EndoKardo@users.noreply.github.com>
2024-02-18 15:49:35 +0300

    Update fractals.py

--- program/fractals.py
+++ program/fractals.py
@@ -1,23 +1,23 @@
-class Fractals:
-  def __init__(self):
+#class Fractals:
+  #def __init__(self):
     # Инициализация модуля фракталов
 
-  def build_fractl(self):
+  #def build_fractl(self):
     # Построение фрактала
 
-  def setting_build_fractl():
+  #def setting_build_fractl():
     # Настройка построения фракталов
 
-  def choose_realization_fractal_build(self):
+  #def choose_realization_fractal_build(self):
     # Выбор  реализации фрактального построения
 
-  def algorithmic_build(self):
+  #def algorithmic_build(self):
     # Алгоритмическое построение фрактала
 
-  def geomethric_build(self):
+  #def geomethric_build(self):
     # Геометрическое построение фрактала
 
-  def IFS_build(self):
+  #def IFS_build(self):
     # Построение фрактала на основе IFS
 
 class NewImagineClass:
