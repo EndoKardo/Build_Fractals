61ccc7c68d14780c01277b3491783079887e07bc
EndoKardo <159020848+EndoKardo@users.noreply.github.com>
2024-02-18 15:31:13 +0300

    Update communication_with_developers.py

--- program/communication_with_developers.py
+++ program/communication_with_developers.py
@@ -1,1 +1,9 @@
+class Communication_with_developers:
+  def __init__(self):
+    # Инициализация для связи с разработчиками
 
+  def proposal_for_innovation(self):
+    # Модуль для предложения разработчикам каких-либо нововведений
+
+  def contact_for_question(self):
+    # Модуль для связи с разработчиками с целью задать им какой-либо вопрос
