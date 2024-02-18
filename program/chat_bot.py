64308f6427a637183b0bd7c1f0e22c3c41111a49
EndoKardo <159020848+EndoKardo@users.noreply.github.com>
2024-02-18 15:28:20 +0300

    Update chat_bot.py

--- program/chat_bot.py
+++ program/chat_bot.py
@@ -1,1 +1,9 @@
+class Chat_bot:
+  def __init__(self):
+    # Инициализация чат-бота
 
+  def get_answer(self):
+    # Модуль для ответов юзерам от чат-бота
+
+  def fast_work_with_project(self):
+    # Модуль для быстрой работы с проектом с помощью чат-бота
