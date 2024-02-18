2f2241fdd5c4054422968e630080cead56c72423
EndoKardo <159020848+EndoKardo@users.noreply.github.com>
2024-02-18 15:44:13 +0300

    Create fractals.py

---
+++ program/fractals.py
@@ -0,0 +1,38 @@
+class Fractals:
+  def __init__(self):
+    # Инициализация модуля фракталов
+
+  def build_fractl(self):
+    # Построение фрактала
+
+  def setting_build_fractl():
+    # Настройка построения фракталов
+
+  def choose_realization_fractal_build(self):
+    # Выбор  реализации фрактального построения
+
+  def algorithmic_build(self):
+    # Алгоритмическое построение фрактала
+
+  def geomethric_build(self):
+    # Геометрическое построение фрактала
+
+  def IFS_build(self):
+    # Построение фрактала на основе IFS
+
+class NewImagineClass:
+  def __init__(self):
+    print("Hello GIT!")
+    
+class patterns_fractals:
+  def __init__(self):
+    # Инициализация модуля выбора шаблона фрактала
+  
+  def standart_patterns(self):
+    # Стандартные шаблоны фракталов
+
+  def new_patterns(self):
+    # Новые шаблоны фракталов(от пользователей)
+
+  def patterns_from_scrath(self):
+    # Построения шаблона с нуля
